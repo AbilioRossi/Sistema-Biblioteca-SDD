ab52b89ddce06782938617a7f3d559d77f98f51d
Abilio Rossi <abilio@abilio-rossi.bbrouter>
2026-06-27 14:02:13 -0400

    feat(loans): finaliza inicialização do projeto e dependências base

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  clearMocks: true,
+};
+
+export default config;
